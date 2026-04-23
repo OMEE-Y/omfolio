313f8231fd09bc519e6b5ec97cb0936489d27707
OMEE-Y <yewaleom394@gmail.com>
2026-04-24 02:36:33 +0530

    improved about section added what i learn

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -9,20 +9,26 @@ export default function AboutSection() {
         about
       </h2>
       <div className="space-y-5 text-[15.5px] sm:text-[16px] leading-7 sm:leading-8 max-w-2xl text-zinc-700 dark:text-zinc-300">
-        <p>
-          Hey, I'm Om, a full stack developer who loves building clean, modern
-          websites and apps where design, functionality, and even the smallest
-          details matter.
-        </p>
+  <p>
+    Hey, I'm Om Yewale, a full stack developer who loves building clean, modern
+    websites and apps where design, functionality, and even the smallest
+    details matter.
+  </p>
 
-        <p>
-          Tech stack isn't my concern, I'm flexible with whatever the project
-          needs, though I prefer modern frameworks and tools.
-        </p>
+  <p>
+    Tech stack isn't my concern, I'm flexible with whatever the project
+    needs, though I prefer modern frameworks and tools.
+  </p>
 
-       
-        
-      </div>
+  <p>
+    My main tech stack is <strong>Next.js</strong> with <strong>Tailwind CSS</strong> for styling,
+   For  backend  <strong>Express.js</strong>, and databases like <strong>MongoDB</strong> or <strong>MySQL</strong>.
+  </p>
+
+  <p>
+    In my free time, I learn <strong>Python</strong> and read some books or tech blogs
+  </p>
+</div>
     </div>
   );
 }
