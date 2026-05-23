dba2c32a863ecb10ce09d5b44dde9c81c1fc9be5
OMEE-Y <yewaleom394@gmail.com>
2026-05-23 17:47:23 +0530

    improevd robots.ts file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,26 @@ import type { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+  
+        userAgent: ['Googlebot', 'Bingbot', 'Applebot', 'DuckDuckBot'],
+        allow: '/',
+        disallow: '/private/',
+      },
+      {
+        
+        userAgent: ['GPTBot', 'ClaudeBot', 'Google-Extended', 'PerplexityBot'],
+        allow: '/', 
+        disallow: '/private/',
+      },
+      {
+
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
     sitemap: 'https://omeey.vercel.app/sitemap.xml',
   }
 }
